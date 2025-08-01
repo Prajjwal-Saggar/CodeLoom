29850a4b4a90255eace845fcb4d3c0eacb955d43
Prajjwal-Saggar <prajjwalsaggar1@gmail.com>
2025-08-01 23:10:57 +0530

    All set for desktop

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -10,14 +10,14 @@ const Navbar = () => {
         <DynamicFormIcon fontSize="large" className="text-primary " />
       </div>
       <div className="flex  p-1 w-[20%] gap-2 justify-around tracking-wide">
-        <button className="relative overflow-hidden group bg-primary pt-1 pb-1 pl-2 pr-2 rounded-sm w-[48%] cursor-pointer text-white transition-all duration-300 ease-in-out hover:scale-105">
+        <button onClick={()=>{window.open("https://portfolio-lime-nine-10.vercel.app/")}} className="relative overflow-hidden group bg-primary pt-1 pb-1 pl-2 pr-2 rounded-sm w-[48%] cursor-pointer text-white transition-all duration-300 ease-in-out hover:scale-105">
           <span className="relative z-10 flex items-center justify-center">
             Portfolio <InsertLinkIcon fontSize="small" className="ml-1" />
           </span>
           <span className="absolute -top-1/2 left-1/2 w-40 h-40 bg-white opacity-10 rounded-full transform -translate-x-1/2 scale-0 group-hover:scale-150 transition-transform duration-700 ease-out"></span>
         </button>
 
-        <button className="relative overflow-hidden group bg-primary pt-1 pb-1 pl-2 pr-2 w-[48%] rounded-sm cursor-pointer text-white transition-all duration-300 ease-in-out hover:scale-105">
+        <button onClick={()=>{window.open("https://github.com/Prajjwal-Saggar/CodeLoom")}} className="relative overflow-hidden group bg-primary pt-1 pb-1 pl-2 pr-2 w-[48%] rounded-sm cursor-pointer text-white transition-all duration-300 ease-in-out hover:scale-105">
           <span className="relative z-10 flex items-center justify-center">
             Github <GitHubIcon fontSize="small" className="ml-1" />
           </span>
